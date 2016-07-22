fa05a10beedf10fa0025625e07d51903b2a11b09
samanthatun <tunsam@berkeleyprep.org>
2016-07-22 11:25:09 -0400

    More accurate o2diff
    made the diffusion1.m into a function

---
+++ Diffusion.m
@@ -0,0 +1,49 @@
+function [ O2diff ] = Diffusion( x,y,t )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+mkdir('diffusionfig1');
+fig = figure('Color',[0.5 0.5 0.5]);
+
+xmin = -100;
+xmax = -xmin;
+ymin = xmin;
+ymax = xmax;
+
+h= 2;
+Nx = 1 + round((xmax-xmin)/h);
+Ny = 1 + round((ymax-ymin)/h);
+
+dt = 0.3;   %time step
+D= 5.7;       %diffusion coefficient
+
+
+
+O2diff = zeros(Nx,Ny);
+% O2diff(11,18) = 15;   %source of O2diff
+% O2diff(18,13) = 15;
+
+
+for iter =1:10000
+%     O2diff(18,18) = 15;
+%     O2diff(11,13) = 15;
+
+    O2diff(:,1) = 5.7;
+
+
+    for i=2:Nx-1
+        for j=2:Ny-1
+            O2diff(i,j) = O2diff(i,j) + ((D*dt)/(h*h))*...
+                (O2diff(i-1,j)+O2diff(i+1,j)+O2diff(i,j-1)+O2diff(i,j+1)- 4*O2diff(i,j));
+        end
+    end
+
+end
+contourf(xmin:h:xmax, ymin:h:ymax, O2diff,[0:0.1:2], 'edgecolor', 'none');
+    axis equal;
+    colormap(hsv)
+    colorbar;
+    pause(0.01);
+    %fileName=['diffusionfig1/fig_',num2str(iter)];
+    %saveas (fig, fileName, 'jpg');
+end %end of loop for 1:100
